bf97ea1f59639858b648761004b7de4d35c3b837
uday tank <89202753+udaytank22@users.noreply.github.com>
2025-06-30 16:17:09 +0530

    Update ProtectedRoute.js

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -1,5 +1,6 @@
 // src/pages/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
+import { getToken } from "../utils/auth";
 
 export default function ProtectedRoute({ children }) {
     const token = getToken();
