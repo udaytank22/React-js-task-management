4d4515002e6c1f7fe3ac086804d59683530dfee8
uday tank <89202753+udaytank22@users.noreply.github.com>
2025-06-30 13:38:46 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { taskReducer, projectReducer } from "./taskSlice";
+import { taskReducer, projectReducer, authReducer } from "./taskSlice";
 
 export const store = configureStore({
   reducer: {
+    authReducer: authReducer,
     tasks: taskReducer,
     projects: projectReducer,
   },
