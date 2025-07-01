8ba5892e444c48c78908f4f8e6b37523d6016778
uday tank <89202753+udaytank22@users.noreply.github.com>
2025-07-01 19:18:27 +0530

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 import { FadeIn, FadeInBottom, FadeInWords } from "../component/Animations";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
